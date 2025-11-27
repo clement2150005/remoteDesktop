2f94eec95639062d0baa28a5a01f72316bcf9c3e
Clement Colin <clmnt.co@gmail.com>
2025-11-27 10:13:34 +0900

    Quick commit

---
+++ client/build/CMakeFiles/Client_autogen_timestamp_deps.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client_autogen_timestamp_deps"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Client_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server/build/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Server_autogen.dir/ParseCache.txt"
+  "Server_autogen"
+  "CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Server.dir/src/Config/ConfigFileParser.cpp.o"
+  "CMakeFiles/Server.dir/src/Config/ConfigFileParser.cpp.o.d"
+  "CMakeFiles/Server.dir/src/Network/Network.cpp.o"
+  "CMakeFiles/Server.dir/src/Network/Network.cpp.o.d"
+  "CMakeFiles/Server.dir/src/Screenshot/Screenshot.cpp.o"
+  "CMakeFiles/Server.dir/src/Screenshot/Screenshot.cpp.o.d"
+  "CMakeFiles/Server.dir/src/main.cpp.o"
+  "CMakeFiles/Server.dir/src/main.cpp.o.d"
+  "Server"
+  "Server.pdb"
+  "Server_autogen/mocs_compilation.cpp"
+  "Server_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
