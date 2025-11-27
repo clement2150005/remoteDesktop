572f692a3ea9fcd78539aa395d7d37203dfbf05d
Clement Colin <clmnt.co@gmail.com>
2025-11-27 14:24:44 +0900

    Quick commit

--- server/build/CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.o.d
+++ server/build/CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.o.d
@@ -1,3 +1,3 @@
 CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.o: \
- /project/server/build/Server_autogen/mocs_compilation.cpp \
+ /home/clementLinux/repojump/clement2150005/remoteDesktop/server/build/Server_autogen/mocs_compilation.cpp \
  /usr/include/stdc-predef.h
--- server/build/cmake_install.cmake
+++ server/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /project/server
+# Install script for directory: /home/clementLinux/repojump/clement2150005/remoteDesktop/server
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/project/server/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/clementLinux/repojump/clement2150005/remoteDesktop/server/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
