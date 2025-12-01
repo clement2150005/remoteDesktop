8af164ab8654674a5e39c7cc8188f0c3b0c5055f
Clement Colin <clmnt.co@gmail.com>
2025-12-01 16:47:42 +0900

    Quick commit

---
+++ client/build/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Client_autogen.dir/ParseCache.txt"
+  "Client_autogen"
+  "CMakeFiles/Client.dir/Client_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Client.dir/Client_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Client.dir/src/MjpegClient/MjpegClient.cpp.o"
+  "CMakeFiles/Client.dir/src/MjpegClient/MjpegClient.cpp.o.d"
+  "CMakeFiles/Client.dir/src/main.cpp.o"
+  "CMakeFiles/Client.dir/src/main.cpp.o.d"
+  "Client"
+  "Client.pdb"
+  "Client_autogen/mocs_compilation.cpp"
+  "Client_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ client/build/Client_autogen/URJZC7WAD2/moc_MjpegClient.cpp
@@ -0,0 +1,101 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MjpegClient.hpp'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../src/MjpegClient/MjpegClient.hpp"
+#include <QtGui/qtextcursor.h>
+#include <QtNetwork/QSslError>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MjpegClient.hpp' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_MjpegClient_t {
+    uint offsetsAndSizes[2];
+    char stringdata0[12];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_MjpegClient_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_MjpegClient_t qt_meta_stringdata_MjpegClient = {
+    {
+        QT_MOC_LITERAL(0, 11)   // "MjpegClient"
+    },
+    "MjpegClient"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_MjpegClient[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MjpegClient::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_MjpegClient.offsetsAndSizes,
+    qt_meta_data_MjpegClient,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_MjpegClient_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MjpegClient, std::true_type>
+    >,
+    nullptr
+} };
+
+void MjpegClient::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    (void)_o;
+    (void)_id;
+    (void)_c;
+    (void)_a;
+}
+
+const QMetaObject *MjpegClient::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MjpegClient::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MjpegClient.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int MjpegClient::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
